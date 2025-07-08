b23b8f6578b2467d6b7e49c27e0ec06046def3d6
Shahroz-Codes <shafqatshahroz@gmail.com>
2025-07-08 09:44:39 +0500

    Add initial implementation of Math module and Hello script

---
+++ NodeJS/Pyush/1NodeModules/Hello.js
@@ -0,0 +1,13 @@
+
+//Importing Node Modules
+
+const math = require("./Math")
+//  const {sub,add} = require("./Math")
+
+// console.log('Math add 3,4 value is : ', math.add(3,4));
+// console.log('Math sub 3,2 value is : ', math.sub(3,2));
+
+// console.log('Math add 3,4 value is : ', add(3,4));
+// console.log('Math sub 3,2 value is : ', sub(3,2));
+
+console.log(math);
---
+++ NodeJS/Pyush/1NodeModules/Math.js
@@ -0,0 +1,19 @@
+
+//Creating Functions or Modules
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const add = (a, b) => {
+    return a + b
+}
+
+//Exporting Node Modules
+
+// module.exports = {
+//     add,
+//     sub,
+// } 
+
+exports.add2 = (a, b) => a + b;
